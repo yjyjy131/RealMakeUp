475af3b4c86d5a1ea806d472ec36eb2daace08de
Ychoib <51788100+Ychoib@users.noreply.github.com>
2020-10-25 07:28:28 +0900

    Update SkinSettingFragment.java
    Add environment intent --> to skindetection

--- app/src/main/java/com/example/realmakeup/ui/SkinSetting/SkinSettingFragment.java
+++ app/src/main/java/com/example/realmakeup/ui/SkinSetting/SkinSettingFragment.java
@@ -5,9 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -29,6 +30,29 @@ public class SkinSettingFragment extends Fragment {
                 ViewModelProviders.of(this).get(SkinSettingViewModel.class);
         View root = inflater.inflate(R.layout.fragment_skin_setting, container, false);
 
+        Spinner spinner = (Spinner)root.findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), com.example.realmakeup.skindetection.class);
+                String select = (String) parent.getItemAtPosition(position);
+                if(select == "어두운 실내"){
+                    intent.putExtra("environment","dark_inside");
+                }
+                else if (select == "실외"){
+                    intent.putExtra("environment","outside");
+                }
+                else{
+                    intent.putExtra("environment","bright_inside");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), com.example.realmakeup.skindetection.class);
+                intent.putExtra("environment","bright_inside");
+            }
+        });
         Button Button1 = (Button)root.findViewById(R.id.autobutton);
         Button1.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
