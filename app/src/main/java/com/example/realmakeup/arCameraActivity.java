eb99b2183646b6c2bf10c4a82a8f82f5fb2ecd9e
Unknown <fulmeach787@gmail.com>
2020-10-25 15:54:11 +0900

    gitignore setting

---
+++ app/src/main/java/com/example/realmakeup/arCameraActivity.java
@@ -0,0 +1,15 @@
+package com.example.realmakeup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class arCameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ar_camera);
+        
+    }
+}
